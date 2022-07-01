4c3270e0ffd9e56345795b7b63e35187f05282f7
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-07-01 15:28:12 -0500

    [UPDATE]

---
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,13 @@
+#include "rb_trees.h"
+
+/**
+* rb_tree_node - red black tree node
+* @parent: node
+* @int: value
+* @color: node color
+* Return: node or NULL
+*/
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+
+}
