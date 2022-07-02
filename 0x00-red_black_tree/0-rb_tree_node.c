c86dfde9e9706eedd624c361e55f16783b21dc37
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-07-01 19:41:35 -0500

    [UPDATE] task 0

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -1,13 +1,29 @@
 #include "rb_trees.h"
+#include <stdlib.h>
 
 /**
 * rb_tree_node - red black tree node
 * @parent: node
-* @int: value
+* @value: value
 * @color: node color
 * Return: node or NULL
 */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
+	rb_tree_t *newnode;
 
+	newnode = malloc(sizeof(rb_tree_t));
+	if (newnode)
+	{
+		newnode->n = value;
+		newnode->color = color;
+		newnode->parent = parent;
+		newnode->left = NULL;
+		newnode->righht = NULL;
+	}
+	else
+	{
+		newnode = NULL;
+	}
+	return (newnode);
 }
