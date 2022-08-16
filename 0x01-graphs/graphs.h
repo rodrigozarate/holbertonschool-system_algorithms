6c424e0b64b41a9a0f821d6bb7c50499a1f2407d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-08-16 18:23:38 -0500

    [UPDATE]

--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -66,8 +66,8 @@ typedef struct graph_s
 	vertex_t    *vertices;
 } graph_t;
 
-void graph_display(const graph_t *graph);
 graph_t *graph_create(void);
 vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type);
 
 #endif /* _GRAPHS_H_ */
