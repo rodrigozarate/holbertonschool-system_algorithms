cfb32c3b206013c448f1a420b0450f1005d682c1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-09-11 12:50:31 -0500

    [CREATE] point 7

---
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -0,0 +1,11 @@
+#include "huffman.h"
+
+/**
+ * huffman_extract_and_insert - extract and insert
+ * @priority_queue: heap_t
+ * Return: int
+ */
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+
+}
